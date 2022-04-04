9cccf6332e1dd559b28ca3f155999c39b298e77e
Сотникова Белла <be.sotnikova@crpt.ru>
2022-04-04 16:54:25 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//Задание 1. Мили (обязательное к выполнению)
+public class Main {
+    public static void main(String[] args){
+        int ticket = 20000;
+        int mile = 100;
+        int miles = ticket / mile;
+        System.out.println(miles);
+    }
+}
